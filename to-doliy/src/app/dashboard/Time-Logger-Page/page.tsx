07330965efba5aca252d9da1cff9b6f6f8e43dd6
TCJay <123647533+TCAppend@users.noreply.github.com>
2025-08-17 04:39:17 +0800

    Update page.tsx

--- to-doliy/src/app/dashboard/Time-Logger-Page/page.tsx
+++ to-doliy/src/app/dashboard/Time-Logger-Page/page.tsx
@@ -1,7 +1,60 @@
 export default function Time_Logger() {
 
     return (
-      <div>Time logger page</div>
+      <>
+{/* Top Page*/}
+      <div className="p-4 text-black">
+    <div className="bg-[#F9D965] p-4 rounded-3xl">
+    <div className="flex flex-col sm:flex-row items-start sm:items-center justify-between mb-4 gap-2">
+
+      </div>
+      </ div>
+      </div>
+  {/* Body Page*/}
+    <div className="p-4 text-black">
+    <div className="bg-[#F9D965] p-4 rounded-3xl">
+    <div className="flex flex-col sm:flex-row items-start sm:items-center justify-between mb-4 gap-2">
+      <div className="overflow-x-auto">
+  <table className="table">
+    {/* head */}
+    <thead>
+      <tr>
+        <th></th>
+        <th>Name</th>
+        <th>Job</th>
+        <th>Favorite Color</th>
+      </tr>
+    </thead>
+    <tbody>
+      {/* row 1 */}
+      <tr>
+        <th>1</th>
+        <td>Cy Ganderton</td>
+        <td>Quality Control Specialist</td>
+        <td>Blue</td>
+      </tr>
+      {/* row 2 */}
+      <tr>
+        <th>2</th>
+        <td>Hart Hagerty</td>
+        <td>Desktop Support Technician</td>
+        <td>Purple</td>
+      </tr>
+      {/* row 3 */}
+      <tr>
+        <th>3</th>
+        <td>Brice Swyre</td>
+        <td>Tax Accountant</td>
+        <td>Red</td>
+      </tr>
+    </tbody>
+  </table>
+</div>
+      </div>
+      </ div>
+      </div>
+      </>
+      
     );
 
 }
